550742e52f684092b5b833ff4095c54ce89db4db
Hori-Masayuki <hori.masayuki1132@gmail.com>
2021-03-24 14:02:16 +0900

    InsertStudentActivityへのIntentを追記

--- app/src/main/java/xyz/takablog/customermanagementsystem/MainActivity.java
+++ app/src/main/java/xyz/takablog/customermanagementsystem/MainActivity.java
@@ -2,7 +2,9 @@ package xyz.takablog.customermanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void insertStudent(View view){
+        Intent intent = new Intent(MainActivity.this,InsertStudentActivity.class);
+        startActivity(intent);
     }
 }
