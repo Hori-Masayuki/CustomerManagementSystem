f41b620081511b93757e34845833ceb7862ff91b
Hori-Masayuki <hori.masayuki1132@gmail.com>
2021-03-26 23:15:05 +0900

    Activityの追加

---
+++ app/src/main/java/xyz/takablog/customermanagementsystem/InsertGradesActivity.java
@@ -0,0 +1,21 @@
+package xyz.takablog.customermanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class InsertGradesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_grades);
+    }
+
+    public void back(View view) {
+    }
+
+    public void save(View view) {
+    }
+}
