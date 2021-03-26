e53b233d7052a0eb06933ae9fe2b9e2682b3b020
Hori-Masayuki <hori.masayuki1132@gmail.com>
2021-03-26 23:15:29 +0900

    Activityの追加

---
+++ app/src/main/java/xyz/takablog/customermanagementsystem/SelectGradesActivity.java
@@ -0,0 +1,14 @@
+package xyz.takablog.customermanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectGradesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_grades);
+    }
+}
