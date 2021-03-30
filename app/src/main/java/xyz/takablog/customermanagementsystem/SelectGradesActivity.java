8df8592c23b4e35190dbb01951d049193cfecc5c
Hori-Masayuki <hori.masayuki1132@gmail.com>
2021-03-30 18:16:55 +0900

    activityの追加

--- app/src/main/java/xyz/takablog/customermanagementsystem/SelectGradesActivity.java
+++ app/src/main/java/xyz/takablog/customermanagementsystem/SelectGradesActivity.java
@@ -1,14 +1,62 @@
 package xyz.takablog.customermanagementsystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class SelectGradesActivity extends AppCompatActivity {
+import xyz.takablog.customermanagementsystem.helper.OpenHelper;
+
+public class SelectGradesActivity extends android.app.ListActivity {
+
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_grades);
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SQLiteDatabase database = null;
+
+        try {
+            OpenHelper helper = new OpenHelper(this);
+            database = helper.getReadableDatabase();
+
+            cursor = database.query("students",
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    "year ASC");
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                    R.layout.student_list,
+                    cursor,
+                    new String[]{"name", "year"},
+                    new int[]{R.id.listName,R.id.listYear},
+                    CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+            setListAdapter(adapter);
+        }catch (Exception e){
+        }
+        database.close();
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(this,GradesListActivity.class);
+        intent.putExtra("id",id);
+        startActivity(intent);
     }
 }
